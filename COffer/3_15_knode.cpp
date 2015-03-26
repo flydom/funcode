40e9519accef7bf12c1aed5180f1b95afa07203b
BigFun <fzy0201k@gmail.com>
2015-03-26 21:53:35 +0800

    3_15_knode 相关练习

--- COffer/3_15_knode.cpp
+++ COffer/3_15_knode.cpp
@@ -1,5 +1,61 @@
 // 寻找单链表倒数第K个节点
 
+// 相关练习，求链表中间节点
+ListNode FindMidNode(ListNode* pListHead)
+{
+    if (pListHead == NULL)
+    {   
+        return NULL;
+    }
+
+    if (pListHead->m_pNext == NULL || pListHead->m_pNext->m_pNext == NULL)
+    {   
+        return pListHead;
+    }
+
+    ListNode *pAhead = pListHead->m_pNext->m_pNext;
+    ListNode *pBehind = pListHead->m_pNext;     
+    while (pAhead != NULL && pAhead->m_pNext != NULL)
+    {
+        pBehind = pBehind->m_pNext;
+        pAhead = pAhead->m_pNext->m_pNext;    
+    }
+
+    return pBehind;
+}
+
+// 判断单链表是否环形结构
+bool IsListBeRing(ListNode* pListHead)
+{
+    bool bFlag = false;
+
+    if (pListHead == NULL || pListHead->m_pNext == NULL)
+    {   
+        return false;
+    }
+
+    if (pListHead->m_pNext->m_pNext == pListHead)
+    {   
+        return true;
+    }
+
+    ListNode *pAhead = pListHead->m_pNext->m_pNext;
+    ListNode *pBehind = pListHead->m_pNext;  
+    while (pAhead != NULL && pAhead->m_pNext != NULL)
+    {
+        if (pAhead == pBehind)
+        {
+            bFlag = true;
+            break;
+        }
+        
+        pBehind = pBehind->m_pNext;
+        pAhead = pAhead->m_pNext->m_pNext;    
+    }
+
+    return bFlag;
+}
+
 // 方法一
 ListNode* FindKthToTail(ListNode* pListHead, unsigned int k)
 {
